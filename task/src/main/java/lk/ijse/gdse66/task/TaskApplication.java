d6a2452d87bed51325dda1f2fdbfed5d3123e060
maduranga <madurangasadaruwan393@gmail.com>
2024-08-25 10:51:22 +0530

    seven

--- task/src/main/java/lk/ijse/gdse66/task/TaskApplication.java
+++ task/src/main/java/lk/ijse/gdse66/task/TaskApplication.java
@@ -30,7 +30,28 @@ public class TaskApplication implements CommandLineRunner {
 		System.out.println(userList);
 
 
-		String update = userService.updateUser("user1234", "name:maduranga");
+
+		String json="{\n" +
+				"    \"uid\": \"user1234\",\n" +
+				"    \"email\": \"maduranga@example.com\",\n" +
+				"    \"given_name\": \"madu\",\n" +
+				"    \"middle_name\": \"madurur\",\n" +
+				"    \"name\": \"John Michael Doe\",\n" +
+				"    \"family_name\": \"Doe\",\n" +
+				"    \"nickname\": \"Johnny\",\n" +
+				"    \"phone_number\": \"+1234567890\",\n" +
+				"    \"comment\": \"New user account created for testing.\",\n" +
+				"    \"picture\": \"https://example.com/profile-picture.jpg\",\n" +
+				"    \"directory\": \"default\",\n" +
+				"    \"metadata\": {\n" +
+				"        \"color\": \"blue\",\n" +
+				"        \"size\": \"XL\"\n" +
+				"    },\n" +
+				"    \"tags\": [\"admin\", \"beta-tester\"],\n" +
+				"    \"is_suspended\": false,\n" +
+				"    \"is_bot\": false\n" +
+				"}\n";
+		String update = userService.updateUser("user1234", json);
 		System.out.println(update);
 
 //		String delete = userService.deleteUser("user1234");
