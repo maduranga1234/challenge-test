09da37743a883c159ffd53def478a5d03923d03c
maduranga <madurangasadaruwan393@gmail.com>
2024-08-25 10:45:39 +0530

    six

--- task/src/main/java/lk/ijse/gdse66/task/TaskApplication.java
+++ task/src/main/java/lk/ijse/gdse66/task/TaskApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class TaskApplication implements CommandLineRunner {
 
 	@Autowired
-	private UserService userService;  // Make sure userService is private
+	private UserService userService;  // Ensure userService is private
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
@@ -20,10 +22,18 @@ public class TaskApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		String user = userService.createUser();
 		System.out.println(user);
-		String userUpdate=userService.getUserDetails("user1234");
-		System.out.println(userUpdate);
-//		userService.listUsers();
-//		userService.updateUser("user1234","name:maduranga");
-//		userService.deleteUser("user1234");
+
+		String getUserDetails = userService.getUserDetails("user1234");
+		System.out.println(getUserDetails);
+
+		String userList = userService.listUsers();
+		System.out.println(userList);
+
+
+		String update = userService.updateUser("user1234", "name:maduranga");
+		System.out.println(update);
+
+//		String delete = userService.deleteUser("user1234");
+//		System.out.println(delete);
 	}
 }
